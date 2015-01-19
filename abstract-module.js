be67766601b756a308bad760ff09222e0f3718fd
Guillaume Louvigny <glouvigny@users.noreply.github.com>
2015-01-19 21:01:11 +0100

    Abstract Module: Added settings loading/saving methods

--- abstract-module.js
+++ abstract-module.js
@@ -1,11 +1,34 @@
 define(function (require, exports, module) {
+    var Persistance = require('./browser').get('persistance');
+
     var Abstract = function (router) {
+        this.id = null;
         this.name = 'Unnamed module';
         this.icon = 'icon-block';
 
         this.disableable = false;
         this.enabled = false;
         this.router = router;
+        this.context = {};
+    };
+
+    Abstract.prototype.loadSettings = function () {
+        if (this.id === null || this.context.settings === undefined) {
+            return;
+        }
+
+        Persistance.get('settings_' + this.id, this.context.settings)
+            .then(function (settings) {
+                this.context.settings = settings;
+            }.bind(this));
+    };
+
+    Abstract.prototype.saveSettings = function () {
+        if (this.id === null || this.context.settings === undefined) {
+            return;
+        }
+
+        Persistance.set('settings_' + this.id, this.context.settings);
     };
 
     module.exports = Abstract;
